3cc9a351a3d5da10f08d3bb09771d606e71cbf86
Trista McCleary <trista_mccleary@yahoo.com>
2018-03-23 14:41:31 -0400

    add html, script

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  var magic8ball = {};
+  magic8ball.listofanswers = [
+    "Most likely",
+    "Without a doubt",
+    "Not so sure",
+    "Absolutely not",
+    "Unclear, ask again",
+    "Yes",
+    "No",
+    "Never",
+    "Cannot predict now",
+    "Very doubtful",
+    "Signs point to yes",
+    "Possible, but not probable",
+    "Outlook is good",
+  ];
+
+
+});
